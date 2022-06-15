95d84ead476752cb3ca069e91256b217f66c5120
Joaquin Velazquez Sanchez <joacojejox@gmail.com>
2022-06-15 18:27:48 -0300

    Primer commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    let databox: Element= document.getElementById("data")!
+    let redesbox: Element= document.getElementById("redes")!
+    let databoxrigth: NodeListOf<Element>= document.querySelectorAll(".datarigth")!    
+
+    const cargarimagen = (entries: any[], observer: any)=>{
+      entries.forEach((entry:any)=>{
+        if(entry.isIntersecting){
+          if(entry.target.classList.contains("datarigth")){
+          entry.target.classList.add("dataanimatedrigth")
+        }
+        else if(entry.target.getAttribute("id") == "redes"){
+          entry.target.classList.add("redesanimated")
+        }
+        else {
+          entry.target.classList.add("dataanimated")
+        }
+        }
+      })
+    }
+    let observer = new IntersectionObserver(cargarimagen,{
+      root: null,
+      threshold: 0.10,
+    });
+    observer.observe(databox)
+    observer.observe(redesbox)
+    databoxrigth.forEach(e=> observer.observe(e))
+  }
   title = 'Curriculum';
 }
