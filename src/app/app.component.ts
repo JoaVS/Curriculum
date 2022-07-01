9d21b347f0ad9386ac13956e0053ca3cc01981be
Joaquin Velazquez Sanchez <joacojejox@gmail.com>
2022-07-01 11:16:24 -0300

    PreUpdateCommit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,22 @@ import { AfterViewInit, Component, OnInit } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements AfterViewInit {
+export class AppComponent implements OnInit, AfterViewInit {
+  language!:any;
+  ngOnInit(): void {
+    if(localStorage.getItem('language')){
+      this.language = localStorage.getItem('language')!
+    }
+    else {
+      this.language = "spanish"
+      localStorage.setItem('language','spanish')
+    }
+    console.log(typeof this.language)
+  }
   ngAfterViewInit(): void {
     let databox: Element= document.getElementById("data")!
     let redesbox: Element= document.getElementById("redes")!
     let databoxrigth: NodeListOf<Element>= document.querySelectorAll(".datarigth")!    
-
     const cargarimagen = (entries: any[], observer: any)=>{
       entries.forEach((entry:any)=>{
         if(entry.isIntersecting){
@@ -33,6 +43,20 @@ export class AppComponent implements AfterViewInit {
     observer.observe(databox)
     observer.observe(redesbox)
     databoxrigth.forEach(e=> observer.observe(e))
+  }
+  changelenguaje(event:any){
+    if(event.target.classList.contains("languageballleft")){
+    localStorage.setItem('language','english')    
+    event.target.classList.replace("languageballleft","languageballrigth")
+    event.target.parentElement.classList.replace("left","rigth")
+    this.language = localStorage.getItem('language')
+    }
+    else {
+      localStorage.setItem('language','spanish')   
+      event.target.classList.replace("languageballrigth","languageballleft")
+      event.target.parentElement.classList.replace("rigth","left")
+      this.language = localStorage.getItem('language')
+    }
   }
   title = 'Curriculum';
 }
